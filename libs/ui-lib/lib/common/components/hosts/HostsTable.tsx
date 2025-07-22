2166a16ec0f2838678f377de6b72d3d23ac0a780
jgyselov <jgyselov@redhat.com>
2025-07-22 13:33:56 +0200

    Tweak infra env host table sorting and filtering

--- libs/ui-lib/lib/common/components/hosts/HostsTable.tsx
+++ libs/ui-lib/lib/common/components/hosts/HostsTable.tsx
@@ -60,20 +60,30 @@ type HostsTableProps = ReturnType<typeof usePagination> &
     | 'variant'
   > & {
     hosts: Host[];
+    relevanceSorted?: boolean;
     skipDisabled?: boolean;
     children: React.ReactNode;
   };
 
-const HostsTable = ({ hosts, skipDisabled, ...rest }: HostsTableProps & WithTestID) => {
+const HostsTable = ({
+  hosts,
+  skipDisabled,
+  relevanceSorted,
+  ...rest
+}: HostsTableProps & WithTestID) => {
   const data = React.useMemo(
     () =>
       (hosts || [])
         .filter((host) => !skipDisabled || host.status !== 'disabled')
-        .sort((a, b) => (a.createdAt && b.createdAt && a.createdAt < b.createdAt ? -1 : 1)),
-    [hosts, skipDisabled],
+        .sort((a, b) =>
+          relevanceSorted ? 0 : a.createdAt && b.createdAt && a.createdAt < b.createdAt ? -1 : 1,
+        ),
+    [hosts, skipDisabled, relevanceSorted],
   );
 
-  return <AITable<Host> getDataId={getHostId} data={data} {...rest} />;
+  return (
+    <AITable<Host> getDataId={getHostId} data={data} relevanceSorted={relevanceSorted} {...rest} />
+  );
 };
 
 export default HostsTable;
